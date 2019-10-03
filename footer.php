97c39c8ea10161bd8b27bdc7b45b5b426eb9bccc
Laura G <valeriogibbs89@gmail.com>
2019-10-02 22:34:21 -0500

    inicial commit

---
+++ footer.php
@@ -0,0 +1,11 @@
+</main>
+        <footer>
+            <p>© 2017 ★ Laura V Gibbs ★ Massachusetts ★ <a  href="http://byui.edu/online">
+            BYU-Idaho Online Learning</a>.</p>
+        </footer>
+      
+      
+      
+      
+    </body>
+</html>
